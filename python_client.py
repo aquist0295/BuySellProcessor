50a3426a9f23f454157aa7100b26ef3673a60a70
aquist0295 <aquist@rocketmail.com>
2023-11-21 14:02:13 -0500

    updated python startup script

--- python_client.py
+++ python_client.py
@@ -1,29 +1,16 @@
-
 from __future__ import print_function
 import webbrowser
 import json
-import logging
 import configparser
 import sys
 import requests
 from rauth import OAuth1Service
-from logging.handlers import RotatingFileHandler
 from accounts.accounts import Accounts
 from market.market import Market
 
 # loading configuration file
 config = configparser.ConfigParser()
 config.read('config.ini')
-
-# logger settings
-logger = logging.getLogger('my_logger')
-logger.setLevel(logging.DEBUG)
-handler = RotatingFileHandler("python_client.log", maxBytes=5*1024*1024, backupCount=3)
-FORMAT = "%(asctime)-15s %(message)s"
-fmt = logging.Formatter(FORMAT, datefmt='%m/%d/%Y %I:%M:%S %p')
-handler.setFormatter(fmt)
-logger.addHandler(handler)
-
 
 def oauth():
     """Allows user authorization for the sample application with OAuth 1"""
@@ -40,8 +27,7 @@ def oauth():
 
 
     # Step 1: Get OAuth 1 request token and secret
-    request_token, request_token_secret = etrade.get_request_token(
-        params={"oauth_callback": "oob", "format": "json"})
+    request_token, request_token_secret = etrade.get_request_token(params={"oauth_callback": "oob", "format": "json"})
 
     # Step 2: Go through the authentication flow. Login to E*TRADE.
     # After you login, the page will provide a verification code to enter.
@@ -50,9 +36,7 @@ def oauth():
     text_code = input("Please accept agreement and enter verification code from browser: ")
 
     # Step 3: Exchange the authorized request token for an authenticated OAuth 1 session
-    session = etrade.get_auth_session(request_token,
-                                  request_token_secret,
-                                  params={"oauth_verifier": text_code})
+    session = etrade.get_auth_session(request_token,request_token_secret,params={"oauth_verifier": text_code})
 
     main_menu(session, base_url)
 
