fa14d601065ac818412d47c22c6bba7fe74b7491
aquist0295 <aquist@rocketmail.com>
2023-11-21 14:01:25 -0500

    market quote script update to update quotes during trading day at 10 sec interval

--- market.py
+++ market.py
@@ -1,64 +1,44 @@
 import json
-import logging
-from logging.handlers import RotatingFileHandler
+import time
 from datetime import datetime
 
-# logger settings
-logger = logging.getLogger('my_logger')
-logger.setLevel(logging.DEBUG)
-handler = RotatingFileHandler("python_client.log", maxBytes=5 * 1024 * 1024, backupCount=3)
-FORMAT = "%(asctime)-15s %(message)s"
-fmt = logging.Formatter(FORMAT, datefmt='%m/%d/%Y %I:%M:%S %p')
-handler.setFormatter(fmt)
-logger.addHandler(handler)
-
-
 class Market:
-    def __init__(self, session, base_url):
-        self.session = session
-        self.base_url = base_url
+ def __init__(self, session, base_url):
+    self.session = session
+    self.base_url = base_url
 
-    def quotes(self):
-        """
-        Calls quotes API to provide quote details for equities, options, and mutual funds
+ def quotes(self):
 
-        :param self: Passes authenticated session in parameter
-        """
-        symbols = input("\nPlease enter Stock Symbol: ")
+    symbols = input("\nPlease enter Stock Symbol: ")
 
-        # URL for the API endpoint
-        url = self.base_url + "/v1/market/quote/" + symbols + ".json"
+    # URL for the API endpoint
+    url = self.base_url + "/v1/market/quote/" + symbols + ".json"
 
-        # Make API call for GET request
+    # Get the current time
+    now = datetime.now()
+    dt_string_Hour    = int(now.strftime("%H"))
+
+    # Make API call for GET request
+    while dt_string_Hour >= 9 and dt_string_Hour <= 16:
         response = self.session.get(url)
-        logger.debug("Request Header: %s", response.request.headers)
 
         if response is not None and response.status_code == 200:
 
             parsed = json.loads(response.text)
-            logger.debug("Response Body: %s", json.dumps(parsed, indent=4, sort_keys=True))
 
             # Handle and parse response
-
-            # Get the current time
-            now = datetime.now()
-            dt_string_Hour    = int(now.strftime("%H"))
-            dt_string_Minutes = int(now.strftime("%M"))
-
-        #    
-        while dt_string_Hour >= 9 and dt_string_Hour <= 16:
-            dt_string_Hour = int(now.strftime("%H"))
+            print("")
             data = response.json()
             if data is not None and "QuoteResponse" in data and "QuoteData" in data["QuoteResponse"]:
-                file1 = open('tickerInfo.txt', 'w')
                 for quote in data["QuoteResponse"]["QuoteData"]:
                     if quote is not None and "Product" in quote and "symbol" in quote["Product"]:
                         print("Symbol: " + quote["Product"]["symbol"])
-                        file1.write(quote["Product"]["symbol"] + " Time: " + str(dt_string_Hour) + " : " + str(dt_string_Minutes))
+                        ticker = quote["Product"]["symbol"]
                     if quote is not None and "All" in quote and "lastTrade" in quote["All"]:
-                        print("Last Price: " + str(quote["All"]["lastTrade"]) + " Time: " + str(dt_string_Hour) + " : " + str(dt_string_Minutes))
-                        file1.write(str(quote["All"]["lastTrade"]))
-                        file1.close()                      
+                        print("Last Price: " + str(quote["All"]["lastTrade"]))
+                        price = quote["Product"]["symbol"]
+                        Stock_info = [ticker, price]
+                        time.sleep(10)
             else:
                 # Handle errors
                 if data is not None and 'QuoteResponse' in data and 'Messages' in data["QuoteResponse"] \
@@ -68,6 +48,9 @@ class Market:
                         print("Error: " + error_message["description"])
                 else:
                     print("Error: Quote API service error")
+                    break        
         else:
-            logger.debug("Response Body: %s", response)
             print("Error: Quote API service error")
+            break
+
+ #def send_info(Stock_info):
