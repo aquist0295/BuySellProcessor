4cbca6023d8c5c25ab957e9a9f197ad63c7121d9
aquist0295 <aquist@rocketmail.com>
2023-11-22 14:15:35 -0500

    This update will print the price of the stock avery time the price changes

--- market.py
+++ market.py
@@ -1,6 +1,6 @@
 import json
-import time
 from datetime import datetime
+#from market_quote import quote
 
 class Market:
  def __init__(self, session, base_url):
@@ -15,42 +15,37 @@ class Market:
     url = self.base_url + "/v1/market/quote/" + symbols + ".json"
 
     # Get the current time
-    now = datetime.now()
-    dt_string_Hour    = int(now.strftime("%H"))
+    Hour    = int(datetime.now().strftime("%H"))
+
+    #set previous price of stock to arbitrary variable at this time
+    old_price  = float(0.0)
 
     # Make API call for GET request
-    while dt_string_Hour >= 9 and dt_string_Hour <= 16:
-        response = self.session.get(url)
+    response = self.session.get(url)
 
-        if response is not None and response.status_code == 200:
-
-            parsed = json.loads(response.text)
-
-            # Handle and parse response
-            print("")
+    if response is not None and response.status_code == 200:
+        while Hour >= 9 and Hour <= 16:
+            #print("")
             data = response.json()
             if data is not None and "QuoteResponse" in data and "QuoteData" in data["QuoteResponse"]:
                 for quote in data["QuoteResponse"]["QuoteData"]:
                     if quote is not None and "Product" in quote and "symbol" in quote["Product"]:
-                        print("Symbol: " + quote["Product"]["symbol"])
                         ticker = quote["Product"]["symbol"]
-                    if quote is not None and "All" in quote and "lastTrade" in quote["All"]:
-                        print("Last Price: " + str(quote["All"]["lastTrade"]))
-                        price = quote["Product"]["symbol"]
-                        Stock_info = [ticker, price]
-                        time.sleep(10)
+                    if quote is not None and "All" in quote and "lastTrade" in quote["All"]:   
+                        new_price = quote["All"]["lastTrade"]
+                        if new_price != old_price: 
+                            print(str(new_price))
+                            old_price = new_price
+                response = self.session.get(url)                
             else:
                 # Handle errors
                 if data is not None and 'QuoteResponse' in data and 'Messages' in data["QuoteResponse"] \
-                        and 'Message' in data["QuoteResponse"]["Messages"] \
-                        and data["QuoteResponse"]["Messages"]["Message"] is not None:
+                         and 'Message' in data["QuoteResponse"]["Messages"] \
+                         and data["QuoteResponse"]["Messages"]["Message"] is not None:
                     for error_message in data["QuoteResponse"]["Messages"]["Message"]:
                         print("Error: " + error_message["description"])
                 else:
                     print("Error: Quote API service error")
                     break        
-        else:
-            print("Error: Quote API service error")
-            break
-
- #def send_info(Stock_info):
+    else:
+        print("Error: Quote API service error")
